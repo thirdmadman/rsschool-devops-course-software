0f4600fccaafee3ba4c591d41e70a4a7bf7b29c7
thirdmadman <thirdmadman@gmail.com>
2024-12-08 20:57:53 +0100

    feat: add separate password secret

--- grafana/deploy_grafana.sh
+++ grafana/deploy_grafana.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
+kubectl create secret generic grafana-admin-secret -n monitoring --from-literal=admin-password=$(openssl rand -base64 8)
+
 kubectl create configmap grafana-dashboard -n monitoring --from-file=/root/grafana/grafana-dashboard.json
 
 helm upgrade --install grafana bitnami/grafana -n monitoring \
@@ -9,4 +11,6 @@ helm upgrade --install grafana bitnami/grafana -n monitoring \
 --set-json 'datasources.secretDefinition={"apiVersion":1,"datasources":[{"name":"Prometheus","type":"prometheus","url":"http://prometheus-kube-prometheus-prometheus:9090","isDefault":true,"uid":"prometheusdatasource"}]}' \
 --set dashboardsProvider.enabled=true \
 --set dashboardsConfigMaps[0].configMapName=grafana-dashboard \
---set dashboardsConfigMaps[0].fileName=grafana-dashboard.json
+--set dashboardsConfigMaps[0].fileName=grafana-dashboard.json \
+--set admin.existingSecret: "grafana-admin-secret" \
+--set admin.existingSecretPasswordKey: admin-password
