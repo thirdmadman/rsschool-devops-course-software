1258a813307edc4598848377c75c6e5c26b72821
thirdmadman <thirdmadman@gmail.com>
2024-12-08 18:26:08 +0100

    fix: correct port

--- prometheus-grafana/deploy_prometheus_grafana.sh
+++ prometheus-grafana/deploy_prometheus_grafana.sh
@@ -5,7 +5,7 @@ kubectl create configmap grafana-dashboard -n monitoring --from-file=grafana-das
 
 helm upgrade --install grafana bitnami/grafana -n monitoring \
 --set service.type="NodePort" \
---set service.nodePorts.grafana="32000" \
+--set service.nodePorts.grafana="32030" \
 --set-json 'datasources.secretDefinition={"apiVersion":1,"datasources":[{"name":"Prometheus","type":"prometheus","url":"http://prometheus-kube-prometheus-prometheus:9090","isDefault":true,"uid":"prometheusdatasource"}]}' \
 --set dashboardsProvider.enabled=true \
 --set dashboardsConfigMaps[0].configMapName=grafana-dashboard \
